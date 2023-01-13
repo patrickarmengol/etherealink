ec9519e3c0e11d5968a0c1b3dc1a2b55a97eb852
Patrick Armengol <patrickarmengol@protonmail.com>
2023-01-13 12:47:59 +0000

    added cors

--- backend/src/etherealink/main.py
+++ backend/src/etherealink/main.py
@@ -1,12 +1,27 @@
 from datetime import datetime
 
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 
 from etherealink import crud, schemas
 from etherealink.database import orm_init
 
 app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 orm_init(app)
 
 
