14b1b379cdcc3cde20156216030f1a9a2d548b2e
Patrick Armengol <patrickarmengol@protonmail.com>
2023-01-14 03:21:56 +0000

    fixed default db_url

--- backend/src/etherealink/config.py
+++ backend/src/etherealink/config.py
@@ -6,7 +6,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     env_name: str = 'local'
     base_url: str = 'http://localhost:5000'
-    db_url: str = 'sqlite:///:memory:'
+    db_url: str = 'sqlite://:memory:'
 
     class Config(BaseSettings.Config):  # https://github.com/pydantic/pydantic/issues/4223
         env_file = '.env'
